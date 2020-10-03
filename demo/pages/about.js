fef7db21c8a1f5f644400223ec33cb976ed359a6
Jon DeWitt <jonathan.c.dewitt@gmail.com>
2020-10-03 00:40:17 -0400

    updating about page with some content

--- demo/pages/about.js
+++ demo/pages/about.js
@@ -5,7 +5,9 @@ export default createContainerComponent(() => {
   return /*html*/`
     <page-container>
       <presentational-link><router-link href="/">Go Back</router-link></presentational-link>
-      
+      <p>Thunderous was mainly inspired by React, with some ideas taken from Vue and Angular.  The idea is to make a library that offers a complete solution using native web components to build applications.</p>
+      <p>This means you don't have to worry about being locked in, and you can even import any native web component seamlessly from anywhere!  This opens up thousands of libraries and open source components, including but not limited to Google's popular Polymer.</p>
+      <p>Putting aside the class "extends" syntax, defining components <i>functionally</i> with Thunderous allows us to reduce unneccessary repitition, track component state, easily assign events to the DOM, and more, all while avoiding some of the issues with inheritance.</p>
     </page-container>
   `
 })
