dd13d39dfc125a95499ce78663be96e1a1175d6a
Jon DeWitt <jon.dewitt@thunder.solutions>
2024-10-12 16:25:59 -0400

    enhancing performance of signals with batching and compares

--- src/signals.ts
+++ src/signals.ts
@@ -1,9 +1,11 @@
-
 export type SignalGetter<T> = () => T;
 export type SignalSetter<T> = (newValue: T) => void;
 export type Signal<T = unknown> = [SignalGetter<T>, SignalSetter<T>];
 
 let subscriber: (() => void) | null = null;
+const updateQueue: Set<() => void> = new Set();
+let isBatchingUpdates = false;
+
 export const createSignal = <T = undefined>(initVal?: T): Signal<T> => {
 	const subscribers = new Set<() => void>();
 	let value = initVal as T;
@@ -14,9 +16,21 @@ export const createSignal = <T = undefined>(initVal?: T): Signal<T> => {
 		return value;
 	};
 	const setter = (newValue: T) => {
+		const isObject = typeof newValue === 'object' && newValue !== null;
+		if (!isObject && value === newValue) return;
 		value = newValue;
 		for (const fn of subscribers) {
-			fn();
+			updateQueue.add(fn);
+		}
+		if (!isBatchingUpdates) {
+			isBatchingUpdates = true;
+			requestAnimationFrame(() => {
+				for (const fn of updateQueue) {
+					fn();
+				}
+				updateQueue.clear();
+				isBatchingUpdates = false;
+			});
 		}
 	};
 	return [getter, setter];
