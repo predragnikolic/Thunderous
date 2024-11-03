741f4b8032a1ca899491300334b7ddc85d71c81d
Jon DeWitt <jon.dewitt@thunder.solutions>
2024-11-03 15:23:19 -0500

    adding syntax highlighting

--- www/src/_components/code.ts
+++ www/src/_components/code.ts
@@ -1,5 +1,7 @@
-import { html, css, customElement } from 'thunderous';
+import { html, css, customElement, createEffect } from 'thunderous';
 import { theme } from '../_styles/theme';
+import hljs from 'highlight.js';
+import { highlight } from '../_styles/highlight';
 
 export const Code = customElement(({ adoptStyleSheet }) => {
 	adoptStyleSheet(theme);
@@ -15,18 +17,44 @@ const codeStyles = css`
 	}
 `;
 
-export const CodeBlock = customElement(({ adoptStyleSheet }) => {
+export const CodeBlock = customElement(({ adoptStyleSheet, attrSignals, connectedCallback, elementRef, refs }) => {
 	adoptStyleSheet(theme);
+	adoptStyleSheet(highlight);
 	adoptStyleSheet(codeBlockStyles);
-	return html`<pre><code><slot></slot></code></pre>`;
+	const [language] = attrSignals.language;
+	connectedCallback(() => {
+		const content = elementRef.innerHTML;
+		const code = refs.code;
+		if (code === null) return;
+		code.innerHTML = content;
+		createEffect(() => {
+			if (language() === null) {
+				code.className = 'no-highlight hljs';
+			} else {
+				code.className = `language-${language()}`;
+			}
+		});
+		hljs.highlightElement(code);
+	});
+	return html`
+		<pre>
+			<code ref="code"></code>
+		</pre>
+		<slot style="display: none"></slot>
+	`;
 });
 
 const codeBlockStyles = css`
 	pre {
 		background-color: rgba(255, 255, 255, 0.05);
 		border-radius: 0.5em;
-		padding: 1em;
 		margin: 0;
 		overflow-x: auto;
+		white-space: initial;
+	}
+	code {
+		padding: 1em;
+		display: block;
+		white-space: pre;
 	}
 `;
