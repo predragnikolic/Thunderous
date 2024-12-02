d9c5af4923b4996b3da748f3b94a0b18c6fa2867
Jon DeWitt <jon.dewitt@thunder.solutions>
2024-12-01 22:14:10 -0500

    docs: making 404 handling more standard

--- www/vite.config.ts
+++ www/vite.config.ts
@@ -1,3 +1,4 @@
+import { existsSync, readFileSync } from 'fs';
 import { glob } from 'glob';
 import { resolve } from 'path';
 import { defineConfig } from 'vite';
@@ -12,7 +13,34 @@ export default defineConfig({
 			input: glob.sync(resolve(__dirname, 'src', '**/*.html')),
 		},
 	},
-	plugins: [ViteEjsPlugin()],
+	plugins: [
+		ViteEjsPlugin(),
+		{
+			name: 'custom-middleware',
+			configureServer(server) {
+				server.middlewares.use((req, res, next) => {
+					const url = req.url ?? '';
+
+					// Skip if the URL has an extension
+					if (url.includes('.') || url.includes('@')) return next();
+
+					const filePath = `${__dirname}/src${url.replace(/\/$/, '')}/index.html`;
+
+					if (existsSync(filePath) && url.endsWith('/')) {
+						next();
+					} else {
+						const notFoundPath = resolve(__dirname, 'src', '404.html');
+						if (existsSync(notFoundPath)) {
+							res.statusCode = 404;
+							res.end(readFileSync(notFoundPath));
+						} else {
+							next();
+						}
+					}
+				});
+			},
+		},
+	],
 	server: {
 		open: true,
 	},
