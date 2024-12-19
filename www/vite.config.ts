e3c25c1db58a2708783d792975e251079bdaee52
Jon DeWitt <jon.dewitt@thunder.solutions>
2024-12-19 11:24:32 -0500

    docs: switching back to inline client scripts to mimic vercel behavior, because a custom vite config is nothing but headaches

--- www/vite.config.ts
+++ www/vite.config.ts
@@ -1,4 +1,4 @@
-import { existsSync, readFileSync } from 'fs';
+import { existsSync, readFileSync, readSync } from 'fs';
 import { glob } from 'glob';
 import { resolve } from 'path';
 import { defineConfig } from 'vite';
@@ -13,39 +13,7 @@ export default defineConfig({
 			input: glob.sync(resolve(__dirname, 'src', '**/*.html')),
 		},
 	},
-	plugins: [
-		ViteEjsPlugin(),
-
-		// This only applies to the dev server. This is just to mimic the behavior of Vercel's static server
-		{
-			name: 'custom-middleware',
-			configureServer(server) {
-				server.middlewares.use((req, res, next) => {
-					const url = req.url?.replace(/\?.*$/, '') ?? '';
-					if (/\.[a-z]{2,}/.test(url)) return next();
-					const potentialPath = resolve(__dirname, 'src', `.${url}/index.html`);
-					const exists = existsSync(potentialPath);
-					if (url.endsWith('/') && exists) return next();
-
-					// redirect to trailing slash if index.html exists
-					// otherwise redirect to 404
-					if (exists) {
-						res.writeHead(301, {
-							Location: `${url}/${req.url?.includes('?') ? '?' + req.url.split('?')[1] : ''}`,
-						});
-						res.end();
-						return;
-					} else {
-						res.writeHead(301, {
-							location: '/404.html',
-						});
-						res.end();
-						return;
-					}
-				});
-			},
-		},
-	],
+	plugins: [ViteEjsPlugin()],
 	server: {
 		open: true,
 	},
