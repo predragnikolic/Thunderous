939efd4822dbe927811e55267ed2f3472759a77b
Jon DeWitt <jonathan.c.dewitt@gmail.com>
2025-04-13 16:08:27 -0400

    extract nested functions and provide missing closure context in arguments instead

--- src/render.ts
+++ src/render.ts
@@ -4,6 +4,8 @@ import type { ElementParent, Styles, SignalGetter, AnyFn } from './types';
 
 export const renderState = {
 	currentShadowRoot: null as ShadowRoot | null,
+	signalMap: new Map<string, SignalGetter<unknown>>(),
+	callbackMap: new Map<string, AnyFn>(),
 };
 
 const logValueError = (value: unknown) => {
@@ -59,43 +61,140 @@ const createNewNode = (value: unknown, parent: ElementParent) => {
 	return new Text('');
 };
 
+// Handle each interpolated value and convert it to a string.
+// Binding is done only after the combined HTML string is parsed into a DocumentFragment.
+const processValue = (value: unknown): string => {
+	if (!isServer && value instanceof DocumentFragment) {
+		const tempDiv = document.createElement('div');
+		tempDiv.append(value.cloneNode(true));
+		return tempDiv.innerHTML;
+	}
+	if (typeof value === 'function' && 'getter' in value && value.getter === true) {
+		const getter = value as SignalGetter<unknown>;
+		const uniqueKey = crypto.randomUUID();
+		renderState.signalMap.set(uniqueKey, getter);
+		let result = getter();
+		if (Array.isArray(result)) {
+			result = result.map((item: unknown) => processValue(item)).join('');
+		}
+		return isServer ? String(result) : `{{signal:${uniqueKey}}}`;
+	}
+	if (typeof value === 'function') {
+		const uniqueKey = crypto.randomUUID();
+		renderState.callbackMap.set(uniqueKey, value as AnyFn);
+		return isServer ? String(value()) : `{{callback:${uniqueKey}}}`;
+	}
+	if (typeof value === 'object' && value !== null) {
+		logValueError(value);
+		return '';
+	}
+	return String(value);
+};
+
+const CALLBACK_BINDING_REGEX = /(\{\{callback:.+\}\})/;
+const LEGACY_CALLBACK_BINDING_REGEX = /(this.getRootNode\(\).host.__customCallbackFns.get\('.+'\)\(event\))/;
+const SIGNAL_BINDING_REGEX = /(\{\{signal:.+\}\})/;
+
+// Bind signals and callbacks to DOM nodes in a DocumentFragment.
+const evaluateBindings = (element: ElementParent, fragment: DocumentFragment) => {
+	for (const child of element.childNodes) {
+		if (child instanceof Text && SIGNAL_BINDING_REGEX.test(child.data)) {
+			const textList = child.data.split(SIGNAL_BINDING_REGEX);
+			const sibling = child.nextSibling;
+			textList.forEach((text, i) => {
+				const uniqueKey = text.replace(/\{\{signal:(.+)\}\}/, '$1');
+				const signal = uniqueKey !== text ? renderState.signalMap.get(uniqueKey) : undefined;
+				const newValue = signal !== undefined ? signal() : text;
+				const newNode = createNewNode(newValue, element);
+
+				// there is only one text node, originally, so we have to replace it before inserting additional nodes
+				if (i === 0) {
+					child.replaceWith(newNode);
+				} else {
+					element.insertBefore(newNode, sibling);
+				}
+
+				// evaluate signals and subscribe to them
+				if (signal !== undefined && newNode instanceof Text) {
+					createEffect(() => {
+						newNode.data = signal() as string;
+					});
+				} else if (signal !== undefined && newNode instanceof DocumentFragment) {
+					createEffect(() => {
+						const result = signal();
+						const nextNode = createNewNode(result, element);
+						if (nextNode instanceof Text) {
+							throw new TypeError(
+								'Signal mismatch: expected DocumentFragment or Array<DocumentFragment>, but got Text',
+							);
+						}
+						let lastSibling = element.lastChild;
+						for (const child of nextNode.children) {
+							const key = child.getAttribute('key');
+							const matchingNode = element.querySelector(`[key="${key}"]`);
+							if (matchingNode === null) continue;
+							lastSibling = matchingNode.nextSibling;
+							child.replaceWith(matchingNode);
+						}
+						element.insertBefore(nextNode, lastSibling);
+					});
+				}
+			});
+		}
+		if (child instanceof Element) {
+			for (const attr of child.attributes) {
+				if (SIGNAL_BINDING_REGEX.test(attr.value)) {
+					const textList = attr.value.split(SIGNAL_BINDING_REGEX);
+					createEffect(() => {
+						let newText = '';
+						let hasNull = false;
+						for (const text of textList) {
+							const uniqueKey = text.replace(/\{\{signal:(.+)\}\}/, '$1');
+							const signal = uniqueKey !== text ? renderState.signalMap.get(uniqueKey) : undefined;
+							const value = signal !== undefined ? signal() : text;
+							if (value === null) hasNull = true;
+							newText += String(value);
+						}
+						if (hasNull && newText === 'null') {
+							child.removeAttribute(attr.name);
+						} else {
+							child.setAttribute(attr.name, newText);
+						}
+					});
+				} else if (LEGACY_CALLBACK_BINDING_REGEX.test(attr.value)) {
+					const getRootNode = child.getRootNode.bind(child);
+					child.getRootNode = () => {
+						const rootNode = getRootNode();
+						return rootNode instanceof ShadowRoot ? rootNode : fragment;
+					};
+				} else if (CALLBACK_BINDING_REGEX.test(attr.value)) {
+					const textList = attr.value.split(CALLBACK_BINDING_REGEX);
+					createEffect(() => {
+						child.__customCallbackFns = child.__customCallbackFns ?? new Map();
+						let uniqueKey = '';
+						for (const text of textList) {
+							const _uniqueKey = text.replace(/\{\{callback:(.+)\}\}/, '$1');
+							if (_uniqueKey !== text) uniqueKey = _uniqueKey;
+							const callback = uniqueKey !== text ? renderState.callbackMap.get(uniqueKey) : undefined;
+							if (callback !== undefined) {
+								child.__customCallbackFns.set(uniqueKey, callback);
+							}
+						}
+						if (uniqueKey !== '') {
+							child.setAttribute(attr.name, `this.__customCallbackFns.get('${uniqueKey}')(event)`);
+						}
+					});
+				}
+			}
+			evaluateBindings(child, fragment);
+		}
+	}
+};
+
 /**
  * A tagged template function for creating DocumentFragment instances.
  */
 export const html = (strings: TemplateStringsArray, ...values: unknown[]): DocumentFragment => {
-	const signalMap = new Map<string, SignalGetter<unknown>>();
-	const callbackMap = new Map<string, AnyFn>();
-
-	// Handle each interpolated value and convert it to a string.
-	// Binding is done only after the combined HTML string is parsed into a DocumentFragment.
-	const processValue = (value: unknown): string => {
-		if (!isServer && value instanceof DocumentFragment) {
-			const tempDiv = document.createElement('div');
-			tempDiv.append(value.cloneNode(true));
-			return tempDiv.innerHTML;
-		}
-		if (typeof value === 'function' && 'getter' in value && value.getter === true) {
-			const getter = value as SignalGetter<unknown>;
-			const uniqueKey = crypto.randomUUID();
-			signalMap.set(uniqueKey, getter);
-			let result = getter();
-			if (Array.isArray(result)) {
-				result = result.map((item: unknown) => processValue(item)).join('');
-			}
-			return isServer ? String(result) : `{{signal:${uniqueKey}}}`;
-		}
-		if (typeof value === 'function') {
-			const uniqueKey = crypto.randomUUID();
-			callbackMap.set(uniqueKey, value as AnyFn);
-			return isServer ? String(value()) : `{{callback:${uniqueKey}}}`;
-		}
-		if (typeof value === 'object' && value !== null) {
-			logValueError(value);
-			return '';
-		}
-		return String(value);
-	};
-
 	// Combine the strings and values into a single HTML string
 	const innerHTML = strings.reduce((innerHTML, str, i) => {
 		let value: unknown = values[i] ?? '';
@@ -107,10 +206,9 @@ export const html = (strings: TemplateStringsArray, ...values: unknown[]): Docum
 		innerHTML += str + String(value === null ? '' : value);
 		return innerHTML;
 	}, '');
-	if (isServer) {
-		// @ts-expect-error // return a plain string for server-side rendering
-		return innerHTML;
-	}
+
+	// @ts-expect-error // return a plain string for server-side rendering
+	if (isServer) return innerHTML;
 
 	// Parse the HTML string into a DocumentFragment
 	const template = document.createElement('template');
@@ -120,104 +218,9 @@ export const html = (strings: TemplateStringsArray, ...values: unknown[]): Docum
 			? template.content
 			: (renderState.currentShadowRoot.importNode?.(template.content, true) ?? template.content);
 
-	const callbackBindingRegex = /(\{\{callback:.+\}\})/;
-	const legacyCallbackBindingRegex = /(this.getRootNode\(\).host.__customCallbackFns.get\('.+'\)\(event\))/;
-	const signalBindingRegex = /(\{\{signal:.+\}\})/;
-	const parseChildren = (element: ElementParent) => {
-		for (const child of element.childNodes) {
-			if (child instanceof Text && signalBindingRegex.test(child.data)) {
-				const textList = child.data.split(signalBindingRegex);
-				const sibling = child.nextSibling;
-				textList.forEach((text, i) => {
-					const uniqueKey = text.replace(/\{\{signal:(.+)\}\}/, '$1');
-					const signal = uniqueKey !== text ? signalMap.get(uniqueKey) : undefined;
-					const newValue = signal !== undefined ? signal() : text;
-					const newNode = createNewNode(newValue, element);
+	// Bind signals and callbacks to the DocumentFragment
+	evaluateBindings(fragment, fragment);
 
-					// there is only one text node, originally, so we have to replace it before inserting additional nodes
-					if (i === 0) {
-						child.replaceWith(newNode);
-					} else {
-						element.insertBefore(newNode, sibling);
-					}
-
-					// evaluate signals and subscribe to them
-					if (signal !== undefined && newNode instanceof Text) {
-						createEffect(() => {
-							newNode.data = signal() as string;
-						});
-					} else if (signal !== undefined && newNode instanceof DocumentFragment) {
-						createEffect(() => {
-							const result = signal();
-							const nextNode = createNewNode(result, element);
-							if (nextNode instanceof Text) {
-								throw new TypeError(
-									'Signal mismatch: expected DocumentFragment or Array<DocumentFragment>, but got Text',
-								);
-							}
-							let lastSibling = element.lastChild;
-							for (const child of nextNode.children) {
-								const key = child.getAttribute('key');
-								const matchingNode = element.querySelector(`[key="${key}"]`);
-								if (matchingNode === null) continue;
-								lastSibling = matchingNode.nextSibling;
-								child.replaceWith(matchingNode);
-							}
-							element.insertBefore(nextNode, lastSibling);
-						});
-					}
-				});
-			}
-			if (child instanceof Element) {
-				for (const attr of child.attributes) {
-					if (signalBindingRegex.test(attr.value)) {
-						const textList = attr.value.split(signalBindingRegex);
-						createEffect(() => {
-							let newText = '';
-							let hasNull = false;
-							for (const text of textList) {
-								const uniqueKey = text.replace(/\{\{signal:(.+)\}\}/, '$1');
-								const signal = uniqueKey !== text ? signalMap.get(uniqueKey) : undefined;
-								const value = signal !== undefined ? signal() : text;
-								if (value === null) hasNull = true;
-								newText += String(value);
-							}
-							if (hasNull && newText === 'null') {
-								child.removeAttribute(attr.name);
-							} else {
-								child.setAttribute(attr.name, newText);
-							}
-						});
-					} else if (legacyCallbackBindingRegex.test(attr.value)) {
-						const getRootNode = child.getRootNode.bind(child);
-						child.getRootNode = () => {
-							const rootNode = getRootNode();
-							return rootNode instanceof ShadowRoot ? rootNode : fragment;
-						};
-					} else if (callbackBindingRegex.test(attr.value)) {
-						const textList = attr.value.split(callbackBindingRegex);
-						createEffect(() => {
-							child.__customCallbackFns = child.__customCallbackFns ?? new Map();
-							let uniqueKey = '';
-							for (const text of textList) {
-								const _uniqueKey = text.replace(/\{\{callback:(.+)\}\}/, '$1');
-								if (_uniqueKey !== text) uniqueKey = _uniqueKey;
-								const callback = uniqueKey !== text ? callbackMap.get(uniqueKey) : undefined;
-								if (callback !== undefined) {
-									child.__customCallbackFns.set(uniqueKey, callback);
-								}
-							}
-							if (uniqueKey !== '') {
-								child.setAttribute(attr.name, `this.__customCallbackFns.get('${uniqueKey}')(event)`);
-							}
-						});
-					}
-				}
-				parseChildren(child);
-			}
-		}
-	};
-	parseChildren(fragment);
 	return fragment;
 };
 
