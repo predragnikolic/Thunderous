e0e17160df11ab6ebfe888c9ce035fde2fbd93bc
Jon DeWitt <jonathan.c.dewitt@gmail.com>
2025-04-24 19:57:34 -0400

    update sandbox to cover missing scenario

--- demo/src/main.ts
+++ demo/src/main.ts
@@ -50,7 +50,11 @@ const MyElement = customElement<{ count: number }>(
 		const [count, setCount] = propSignals.count;
 		setCount(0);
 		const [heading] = attrSignals.heading;
-		const [list, setList] = createSignal(['a', 'b', 'c']);
+		const [list, setList] = createSignal([
+			{ id: 1, name: 'item 1' },
+			{ id: 2, name: 'item 2' },
+			{ id: 3, name: 'item 3' },
+		]);
 
 		const redValue = derived(() => {
 			const value = count() * 10;
@@ -91,8 +95,19 @@ const MyElement = customElement<{ count: number }>(
 			}
 		`);
 
+		let i = 1;
 		const addListItem = () => {
-			setList([...list(), 'new item']);
+			const _list = list().map((item) => ({ id: item.id, name: `updated: ${item.name.replace('updated: ', '')}` }));
+			setList([..._list, { id: i + 3, name: 'new item ' + i++ }]);
+		};
+
+		const removeListItem = (id: number) => {
+			const _list = list();
+			_list.splice(
+				_list.findIndex((item) => item.id === id),
+				1,
+			);
+			setList(_list);
 		};
 
 		return html`
@@ -107,9 +122,12 @@ const MyElement = customElement<{ count: number }>(
 			<h2>nested templates and loops:</h2>
 			<ul>
 				${html`<li onclick="${addListItem}">item</li>`}
-				${derived(() => list().map((item, i) => html`<li key="${item}-${i}" onclick="${addListItem}">${item}</li>`))}
+				${derived(() =>
+					list().map((item) => html`<li key="${item.id}" onclick="${() => removeListItem(item.id)}">${item.name}</li>`),
+				)}
 				${list().map((item) => html`<li key="${item}">${item} after</li>`)}
 			</ul>
+			<button onclick="${addListItem}">Add List Item</button>
 			<h2>Test</h2>
 			<div><span>test custom getter: </span>${getter(() => 'TESTING CUSTOM GETTER')}</div>
 		`;
